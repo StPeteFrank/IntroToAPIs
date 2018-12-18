594d4a77a684a0d76cb353b52a0587228d2f14bd
Frank Diaz <diazfrankp@gmail.com>
2018-12-18 15:00:23 -0500

    Create Model and class. Added Dbset to comtext.

---
+++ SafariVacationContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using IntroToAPIs.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace IntroToAPIs
+{
+  public partial class SafariVacationContext : DbContext
+  {
+    public SafariVacationContext()
+    {
+    }
+
+    public SafariVacationContext(DbContextOptions<SafariVacationContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+
+        optionsBuilder.UseNpgsql("server=localhost;database=SafariVacation");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+    }
+    public DbSet<SeenAnimals> SeenAnimals { get; set; }
+  }
+}
